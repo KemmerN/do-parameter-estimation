c0cfe53443aea508ca84e910add1837881412436
KemmerN <75536632+KemmerN@users.noreply.github.com>
2024-09-17 09:26:38 +0200

    created parameter estimator folder struct

---
+++ do_mpc/parameter_estimator/._parameterestimator.py
@@ -0,0 +1,4 @@
+import numpy as np
+import casadi.tools as castools
+from do_mpc.tools._casstructure import _SymVar
+from typing import Union,Tuple
---
+++ do_mpc/parameter_estimator/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+Controller for dynamic systems.
+"""
+
+from ._parameterestimator import ParameterEstimator
+from ._parameterestimatorsettings import ParameterEstimatorSettings
+
